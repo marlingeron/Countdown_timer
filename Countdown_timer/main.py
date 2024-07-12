5bed4f77a836fa50a3e7346a7fef4a356439a810
marlingeron <155311138+marlingeron@users.noreply.github.com>
2024-07-12 15:28:14 +0200

    Update main.py

--- Countdown_timer/main.py
+++ Countdown_timer/main.py
@@ -2,7 +2,7 @@ import time
 
 def countdown(t):
     while t:
-        mins, secs = divmod(t, 60)
+        mins, secs = divmod(t, 50)
         timer = '{:02d}:{:02d}'.format(mins,secs)
         print(timer, end="\r")
         time.sleep(1)
